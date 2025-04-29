3b41b555335d15c76796f1a6132742d8a00baff6
Michel9406 <michel9406@hotmail.com>
2025-04-29 01:26:06 -0300

    :white_check_mark:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router'; 
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
+import { RouterModule } from '@angular/router';
+import { SegundoComponenteComponent } from "./segundo-componente/segundo-componente.component";  
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterModule, PrimeiroComponenteComponent, SegundoComponenteComponent], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'fundamentos-angular';
---
+++ src/app/pipe-customizado/pipe-customizado.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipe-customizado',
+  imports: [],
+  templateUrl: './pipe-customizado.component.html',
+  styleUrl: './pipe-customizado.component.css'
+})
+export class PipeCustomizadoComponent {
+
+}
