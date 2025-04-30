46dd0e9025c41f10a240695bd6572c6cdf3fb116
Michel9406 <michel9406@hotmail.com>
2025-04-30 01:49:16 -0300

    :ok_hand:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.component';
 import { RouterModule } from '@angular/router';
 import { SegundoComponenteComponent } from "./segundo-componente/segundo-componente.component";
-import { PipeCustomizadoComponent } from "./pipe-customizado/pipe-customizado.component";  
+import { PipeCustomizadoComponent } from "./pipe-customizado/pipe-customizado.component";
+import { TwoWayDataBindingComponent } from "./two-way-data-binding/two-way-data-binding.component";  
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterModule, PrimeiroComponenteComponent, SegundoComponenteComponent, PipeCustomizadoComponent], 
+  imports: [RouterModule, PrimeiroComponenteComponent, SegundoComponenteComponent, PipeCustomizadoComponent, TwoWayDataBindingComponent], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
